5ea8adbbd856cd7bbf90808667ebd00e9f7cbec6
Jack Kelly <jgkelly@mit.edu>
2019-05-02 12:43:56 -0400

    Back Prop alg and better orginization

---
+++ SMEM/ExactMatch.py
@@ -0,0 +1,171 @@
+from os import path
+import json
+import random
+
+
+
+class ExactMatch:
+
+    def __init__(self, reference_sequence_file: str, query_sequence_file: str=None):
+        self.ref_seq_file = reference_sequence_file
+
+        self.ref_sequence = None
+        self.ref_size = None
+        self.query_sequence = None
+        self.fm_file = self.ref_seq_file.split(".")[0] + "-FM.json"
+        self.fm_index = {}
+
+        if query_sequence_file is not None:
+            self.load_query(query_sequence_file)
+
+
+    def create_fm_index(self):
+        with open(path.join("data", self.ref_seq_file), "r") as refFile:
+            refFile.readline()
+            self.ref_sequence = ""
+            for line in refFile:
+                self.ref_sequence += line.strip()
+
+        bwt_array, first_index_array, suffix_array = self.create_bwt_matrix()
+
+        o_matrix = self.get_occurance_matrix(bwt_array)
+        c_dic = self.get_count_dic(first_index_array)
+        self.fm_index = {"bwt_array": bwt_array, "suffix_array": suffix_array, "occurance_matrix": o_matrix,
+                         "count_dic": c_dic}
+
+        with open(path.join("data", self.fm_file), "w") as fm_file:
+            fm_file.write(json.dumps(self.fm_index, indent=4, sort_keys=True))
+
+
+    def create_bwt_matrix(self):
+        self.ref_sequence += "$"
+        self.ref_size = len(self.ref_sequence)
+        bwt_matrix = []
+
+        # Rotate
+        for index in range(self.ref_size):
+            bwt_matrix.append(self.ref_sequence[index:] + self.ref_sequence[:index])
+        bwt_matrix.sort()
+
+        bwt_array = []
+        first_index_array = []
+        suffix_array = []
+        for rotation in bwt_matrix:
+            bwt_array.append(rotation[-1])
+            first_index_array.append(rotation[0])
+            suffix_array.append(self.ref_size - rotation.index("$"))
+
+        return bwt_array, first_index_array, suffix_array
+
+    @staticmethod
+    def get_occurance_matrix(bwt_array):
+        o_dic = {}
+        index = 0
+        for item in bwt_array:
+            if item not in o_dic:
+                o_dic[item] = [0]
+            item_list = o_dic[item]
+            while len(item_list) <= index:
+                item_list.append(item_list[-1])
+            item_list[-1] += 1
+
+            index += 1
+
+        # Extend everything to full matrix length
+        for key in o_dic:
+            item_list = o_dic[key]
+            while len(item_list) < index:
+                item_list.append(item_list[-1])
+
+        return o_dic
+
+    @staticmethod
+    def get_count_dic(first_index_array):
+        count_dic = {}
+        index = 0
+        for char in first_index_array:
+            if char not in count_dic:
+                count_dic[char] = index
+            index += 1
+        count_dic[""] = index
+        return count_dic
+
+
+    def load_query(self, query_seq_file):
+        with open(path.join("data", query_seq_file), "r") as q_file:
+            self.query_sequence = ""
+            for line in q_file:
+                self.query_sequence += line.strip()
+
+
+    # Create a query of size "query_size" from the reference sequence
+    def create_query(self, query_size, query_output_file=None):
+        if self.ref_sequence is None:
+            with open(path.join("data", self.ref_seq_file), "r") as refFile:
+                refFile.readline()
+                self.ref_sequence = ""
+                for line in refFile:
+                    self.ref_sequence += line.strip()
+            self.ref_size = len(self.ref_sequence)
+
+        random.seed(0)
+        query_start = random.randint(0, self.ref_size - query_size)
+        query = self.ref_sequence[query_start: query_start + query_size]
+        if query_output_file is not None:
+            with open(path.join("data", query_output_file), "w+") as q_out_f:
+                q_out_f.write(">query from '" + self.ref_seq_file + "' zero-index location: " + str(query_start) + "\n")
+                q_out_f.write(query + "\n")
+
+        self.query_sequence = query
+        return query
+
+
+    # Do exact match back prop and return the locations in the suffix array of matches
+    def exact_match_back_prop(self, query_seq: str):
+        if self.fm_index == {}:
+            try:
+                with open(path.join("data", self.fm_file), "r") as FM_file:
+                    self.fm_index = json.load(FM_file)
+            except FileNotFoundError:
+                print("No FM index file found. Run ExactMatch.createFMIndex to create an FM index.")
+                return
+
+        start = 1
+        end = self.fm_index["count_dic"][""]
+        for char in reversed(query_seq):
+            char_count = self.fm_index["count_dic"][char]
+            if start - 1 <= 0:
+                start = char_count + 1
+            else:
+                start = char_count + 1 + self.fm_index["occurance_matrix"][char][start - 2]  # minus 2 bc zero index
+
+            end = char_count + self.fm_index["occurance_matrix"][char][end - 1]  # minus 1 bc zero index
+
+            # No match in ref seq
+            if start > end:
+                return -1
+
+        return start - 1, end - 1  # return zero indexed start and end
+
+
+    def exact_match(self, query_seq: str=None):
+        if query_seq is None:
+            if self.query_sequence is None:
+                print("No query sequence. Either input sequence, load_file, or generate.")
+                return
+            query_seq = self.query_sequence
+
+        start, end = self.exact_match_back_prop(query_seq)
+
+        matches = []
+        for index in range(start, end + 1):
+            matches.append(self.fm_index["suffix_array"][index])
+
+        matches.sort()
+        return matches
+
+
+# e_match = ExactMatch("mississippi.fa")
+# e_match.create_fm_index()
+# e_match.create_query(3, "query1.fa")
+# print(e_match.exact_match())
--- SMEM/SMEM.py
+++ SMEM/SMEM.py
@@ -1,77 +1,87 @@
-ref = "MISSISSIPPI"
-
-q = "ISS"
-
-def get_suffix_index(x):
-    return (0,4)
-
-def get_SMEMS(query, reference):
-
-    currentIndex = 0
-    smem_table = {}
-
-    while currentIndex < len(query):
-        smem = get_SMEM_at_index(query, reference, currentIndex)
-
-        sequence = smem[0]
-        start_index_suffix = smem[1][0]
-        end_index_suffix = smem[1][1]
-
-        for i in range(start_index_suffix, end_index_suffix):
-            smem_table[i] = sequence
-
-        currentIndex = end_index_suffix + 1
-
-    return smem_table
+from ExactMatch import ExactMatch
 
 
-def get_SMEM_at_index(query, reference, start_index):
-    forward_matches = {}
-    backward_matches = {}
-    SMEM = {}
+class SMEM:
+    def __init__(self, matcher: ExactMatch):
+        self.matcher = matcher
 
-    #forward extend
-    for i in range(start_index+1, len(query)+1):
-        currentSearch = query[start_index:i]
+    def get_suffix_index(self, query):
+        return self.matcher.exact_match_back_prop(query)
 
-        suffix_tuple = get_suffix_index(currentSearch)
 
-        if suffix_tuple == -1:
-            break;
-        else:
-            forward_matches[currentSearch] = suffix_tuple
+    def get_SMEMS(self, query):
 
-    print ("Forward Extension Matches: " + str(forward_matches))
+        currentIndex = 0
+        smem_table = {}
 
-    #backward extend
-    for key in forward_matches:
+        while currentIndex < len(query):
+            smem = self.get_SMEM_at_index(query, currentIndex)
 
-        for i in range(start_index-1, -1, -1):
+            sequence = smem[0]
+            start_index_suffix = smem[1][0]
+            end_index_suffix = smem[1][1]
 
-            currentSearch = query[i: start_index] + key
+            for i in range(start_index_suffix, end_index_suffix):
+                smem_table[i] = sequence
 
-            suffix_tuple = get_suffix_index(currentSearch)
+            currentIndex = end_index_suffix + 1
+
+        return smem_table
+
+
+    def get_SMEM_at_index(self, query, start_index):
+        forward_matches = {}
+        backward_matches = {}
+
+        #forward extend
+        for i in range(start_index+1, len(query)+1):
+            currentSearch = query[start_index:i]
+
+            suffix_tuple = self.get_suffix_index(currentSearch)
 
             if suffix_tuple == -1:
-                break;
+                break
             else:
-                backward_matches[currentSearch] = suffix_tuple
+                forward_matches[currentSearch] = suffix_tuple
 
-    print("Backward Extension Matches: " + str(backward_matches))
+        print("Forward Extension Matches: " + str(forward_matches))
 
-    #get SMEM from backward_matches
-    largest = ''
+        #backward extend
+        for key in forward_matches:
 
-    if len(backward_matches) == 0:
-        for match in forward_matches:
-            if len(match) > len(largest):
-                largest = match
-        return [largest, forward_matches[largest]]
-    else:
-        for match in backward_matches:
-            if len(match) > len(largest):
-                largest = match
+            for i in range(start_index-1, -1, -1):
 
-        return [largest, backward_matches[largest]]
+                currentSearch = query[i: start_index] + key
 
-print (get_SMEMS(q, ref))
+                suffix_tuple = self.get_suffix_index(currentSearch)
+
+                if suffix_tuple == -1:
+                    break
+                else:
+                    backward_matches[currentSearch] = suffix_tuple
+
+        print("Backward Extension Matches: " + str(backward_matches))
+
+        #get SMEM from backward_matches
+        largest = ''
+
+        if len(backward_matches) == 0:
+            for match in forward_matches:
+                if len(match) > len(largest):
+                    largest = match
+            return [largest, forward_matches[largest]]
+        else:
+            for match in backward_matches:
+                if len(match) > len(largest):
+                    largest = match
+
+            return [largest, backward_matches[largest]]
+
+
+
+if __name__ == '__main__':
+    matcher = ExactMatch("mississippi.fa")
+    query = matcher.create_query(2)
+    print(query)
+    smem = SMEM(matcher)
+    print(smem.get_SMEMS(query))
