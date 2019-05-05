6632696ad958590c34fe60ee67959b5eb1f580da
Carlos Garcia <carlosga@mit.edu>
2019-05-05 16:36:22 -0400

    finding subtrings of length m over all positions

--- SMEM/LUT.py
+++ SMEM/LUT.py
@@ -4,3 +4,26 @@ class LUT:
 
     def __init__(self, matcher: ExactMatch):
         self.matcher = matcher
+        self.LUT = None
+
+    def generate_lut(self, size):
+        self.LUT = {}
+        ref = self.matcher.ref_sequence[:self.matcher.ref_size-1]
+
+        for pos in range(self.matcher.ref_size):
+            substrings = []
+
+            for i in range(size):
+                if self.matcher.ref_size - size > pos-i >= 0:
+                    substrings.append(ref[pos-i:pos+size-i])
+
+            if len(substrings) > 0:
+                self.LUT[pos] = substrings
+
+
+matcher = ExactMatch("mississippi.fa")
+matcher.create_fm_index()
+lut = LUT(matcher)
+lut.generate_lut(4)
+
+print(lut.LUT)
